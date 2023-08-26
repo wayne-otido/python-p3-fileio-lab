e46b930fc6201a480190ff6e6e7a13748820fdd2
wayne-otido <otidowayne@gmail.com>
2023-08-26 23:16:53 +0300

    done

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,15 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f"{file_name}.txt", mode = "w", encoding = "utf-8") as file_name:
+        file_name.write(f"{file_content}")
+
+write_file("test_file", "This is a test content")
 
 def append_file(file_name, append_content):
-    pass
+    with open(f"{file_name}.txt", mode = "a", encoding = "utf-8") as file_name:
+        file_name.write(f"{append_content}")
 
 def read_file(file_name):
-    pass
+    with open(f"{file_name}.txt", mode= "r" , encoding = "utf-8") as file_name:
+        return file_name.read()
+
+read_file("test_file")
